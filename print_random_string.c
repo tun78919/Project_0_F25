f40c21f86964332c5c82c2dcc214e44bdbc5d160
Thompson <tun78919@temple.edu>
2025-08-27 10:07:01 -0400

    Function prototype for rand_string()

--- print_random_string.c
+++ print_random_string.c
@@ -1,5 +1,3 @@
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -22,13 +20,13 @@ int main()
 
 {
     char *str;
-    size_t size =   ;  //specify a value for size of word
+    size_t size = 8;  //specify a value for size of word
 
 	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
 
 	printf("Today's random word: ");
-    str = rand_string_alloc(size );
+    str = rand_string_alloc(size);
     printf("%s",str);
     free (str);
     
--- random.c
+++ random.c
@@ -1,7 +1,16 @@
 // Preprocessor Directives
 #include <stdio.h>
 
+// Function Prototypes
+char rand_string(size_t size); 
+
 // Main Function
 int main(void){
     return 0;
+}
+
+// Function for rand_string()
+char rand_string(size_t size){
+    
+    return;
 }
